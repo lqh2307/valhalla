d303fd6317d92b3d6eb8dda1f9d13e4aa9139628
lqh2307 <anhhuyvip2000kaka@gmail.com>
2024-07-30 01:57:57 +0700

    1.0.0

--- scripts/build_and_install.sh
+++ scripts/build_and_install.sh
@@ -3,6 +3,6 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 # switch back to -DCMAKE_BUILD_TYPE=RelWithDebInfo and uncomment the block below if you want debug symbols
-cmake -B build -DCMAKE_BUILD_TYPE=Release -DENABLE_SINGLE_FILES_WERROR=Off
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DENABLE_SINGLE_FILES_WERROR=Off
 make -C build -j${CONCURRENCY:-$(nproc)}
 sudo make -C build install
