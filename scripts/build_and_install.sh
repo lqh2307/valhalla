3ec346e7aee1a7396f8227dce0e7f18c8379b236
lqh2307 <anhhuyvip2000kaka@gmail.com>
2024-08-23 22:31:36 +0700

    1.0.0

--- scripts/build_and_install.sh
+++ scripts/build_and_install.sh
@@ -2,7 +2,14 @@
 
 set -e  # Exit immediately if a command exits with a non-zero status
 
-# switch back to -DCMAKE_BUILD_TYPE=RelWithDebInfo and uncomment the block below if you want debug symbols
+# Decide whether to use sudo
+SUDO_CMD="sudo"
+
+if [ "$NO_USE_SUDO" = "true" ]; then
+  SUDO_CMD=""
+fi
+
+# switch back to -DCMAKE_BUILD_TYPE=RelWithDebInfo if you want debug symbols
 cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DENABLE_SINGLE_FILES_WERROR=Off
 make -C build ${ADDITIONAL_TARGETS} -j${CONCURRENCY:-$(nproc)}
-sudo make -C build install
+$SUDO_CMD make -C build install
