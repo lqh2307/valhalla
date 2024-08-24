cb683411406ba55be34ff9f12f5367c328f7ed84
lqh2307 <168527505+lqh2307@users.noreply.github.com>
2024-08-24 14:05:22 +0700

    1.0.0

--- scripts/build_and_install.sh
+++ scripts/build_and_install.sh
@@ -9,6 +9,8 @@ if [ "$NO_USE_SUDO" = "true" ]; then
   SUDO_CMD=""
 fi
 
+rm -rf build
+
 # switch back to -DCMAKE_BUILD_TYPE=RelWithDebInfo if you want debug symbols
 cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=gcc -DENABLE_SINGLE_FILES_WERROR=Off
 make -C build ${ADDITIONAL_TARGETS} -j${CONCURRENCY:-$(nproc)}
